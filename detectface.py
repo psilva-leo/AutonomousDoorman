a8a23ea1503fcaf6aa97b02cef03954757fa3167
psilva-leo <psilva.leo@gmail.com>
2016-10-26 14:20:30 -0200

    Initial Commit
    Face detection prototype

---
+++ detectface.py
@@ -0,0 +1,24 @@
+import cv2
+
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+
+    for(x, y, w, h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y + h, x:x + h]
+
+    cv2.imwrite('test.jpg', img)
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
