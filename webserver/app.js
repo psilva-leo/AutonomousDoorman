8716d4fcfcd88522d4fbe05e18be9d8f311d6f71
psilva.leo <psilva.leo@gmail.com>
2016-11-14 23:26:17 -0200

    setting up heroku

--- webserver/app.js
+++ webserver/app.js
@@ -1,32 +1,23 @@
 var express = require('express');
 var path = require('path');
+
 var app = express();
-var port = 8000;
 
+app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'ejs');
-app.use('/gentelella', express.static(path.join(__dirname, '/node_modules/gentelella')));
 
-// Home
+app.use('/gentelella', express.static(path.join('node_modules/gentelella')));
+
+//Home
 app.get('/', function(req, res){
 	res.render('home');
 });
 
-// Test
-app.get('/test/:testvar?', function(req, res){
-	var testvar = req.params.testvar;
-	res.send("This is a response on test " + testvar);
-});
 
-// Not founf (404)
 app.get('*', function(req, res){
 	res.render('404');
 });
 
-// Botton of file
-app.listen(port, function(err, res){
-	if(err){
-		console.log('server crashed');
-	}else{
-		console.log('Server started and listening on port 8000');
-	}
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
